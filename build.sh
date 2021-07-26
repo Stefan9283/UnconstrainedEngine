d6dd78d9d18f90bb6d582b0209d77df7aa353a9f
Stefan9283 <34220105+Stefan9283@users.noreply.github.com>
2021-07-26 15:56:19 +0300

    main clear

--- build.sh
+++
@@ -1,3 +0,0 @@
-bash clear.sh
-
-cmake .
--- clear.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/bash
-tobedeleted='CMakeCache* *.vcxproj* x64 Debug CMakeFiles Triangle.sln Triangle.dir .vs cmake_install*'
-    rm -rf ${tobedeleted}
